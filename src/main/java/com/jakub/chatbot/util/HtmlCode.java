07bd90a53e8e4aa228ce69040b2cc5bfe13cffdc
Jakub Kuśnierz <jakubkusnierz@gmail.com>
2018-12-18 00:23:13 +0100

    add layout

--- src/main/java/com/jakub/chatbot/util/HtmlCode.java
+++ src/main/java/com/jakub/chatbot/util/HtmlCode.java
@@ -3,21 +3,21 @@ package com.jakub.chatbot.util;
 public class HtmlCode {
 
 	static String userCode(String message) {
-		return "<div class=\"container darker\">\n" +
+		return "<div class=\"messaging darker\">\n" +
 				"    <img src=\"/img/human.png\" alt=\"Avatar\" class=\"right\" style=\"width:100%;\">\n" +
 				"    <p>" + message + "</p>\n" +
 				"	 </div>";
 	}
 
 	static String botCode(String message) {
-		return "<div class=\"container\">\n" +
+		return "<div class=\"messaging\">\n" +
 				"	 <img src=\"/img/bot.png\" alt=\"Avatar\" style=\"width:100%;\">\n" +
 				"    <p>" + message + "</p>\n" +
 				"</div>";
 	}
 
 	static String botCodeOffTopic(String message) {
-		return "<div class=\"container\">\n" +
+		return "<div class=\"messaging\">\n" +
 				"	 <img src=\"/img/bot.png\" alt=\"Avatar\" style=\"width:100%;\">\n" +
 				"    <p>Twoja odpowiedź jest nie na temat. Postaraj się udzielać bardziej dokładnych odpowiedzi.</p>\n" +
 				"    <p>" + message + "</p>\n" +
@@ -25,7 +25,7 @@ public class HtmlCode {
 	}
 
 	static String endConversation() {
-		return "<div class=\"container\">\n" +
+		return "<div class=\"messaging\">\n" +
 				"	 <img src=\"/img/bot.png\" alt=\"Avatar\" style=\"width:100%;\">\n" +
 				"    <p>Dziękujemy za rozmowe. Aby zobaczyć ocenę przejdź do podsumowania!</p>\n" +
 				"</div>";
